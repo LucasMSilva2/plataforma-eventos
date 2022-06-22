069a20faf8a97fea3e479f61765c3ae38d56e5b4
lucas <lucastorrao691@gmail.com>
2022-06-21 22:00:19 -0300

    segunda aula concluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { gql, useQuery} from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql `
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <Event />
   )
 }
 
 export default App
+
+// CMS = Content Management System
+
+// HeadLess CMS ()
 
 // query / mutation
 
